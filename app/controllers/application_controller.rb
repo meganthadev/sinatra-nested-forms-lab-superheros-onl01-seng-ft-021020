2d38a48821e7bc70facbf6ea2982e8ed1ab1f112
Megan Horsfield <meganthadev@gmail.com>
2020-04-09 05:12:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,23 +4,19 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
     
-    get '/' do 
-      erb :new 
-    end 
-    
-    post '/teams' do 
-      @team = Team.new(params[:team])
-      
-      params[:team][:heros].each do |info|
-        Team.new(info)
-      end 
-      
-      @heros = Hero.all 
-      
-      erb :"/team"
-    end   
-    
-    
+    get '/' do
+        erb :super_hero
+      end
+
+      post '/team' do
+        @team = Team.new(params[:team])
+        params[:team][:heros].each do |hero|
+          Hero.new(hero)
+        end
+
+        @heros = Hero.all
+        erb :team
+      end
 
 
 end
